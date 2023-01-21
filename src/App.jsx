e6437cccc6d9b022946b9c8e118bd73f330c45c1
Tribagasiskandar <bagasiskar10@gmail.com>
2023-01-21 15:13:05 +0700

    bagas
    final task

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pascal from './Pages/Pascal'
+import TwoSum from './Pages/Two'
+
+export default function App() {
+  return (
+    <div>
+      <TwoSum />
+      <Pascal />
+    </div>
+  )
+}
---
+++ src/Pages/Pascal.jsx
@@ -0,0 +1,54 @@
+import { Box, Button, Input, Stack, Typography } from "@mui/material";
+import { useState } from "react";
+
+const Pascal = () => {
+  const [rowCount, setRowCount] = useState();
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const pascalTriangle = (numRows) => {
+    let arr = [];
+
+    for (let i = 0; i < numRows; i++) {
+      arr[i] = [];
+      arr[0] = [1];
+      arr[i][0] = 1;
+
+      for (let j = 1; j < i; j++) {
+        arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+      }
+
+      arr[i][i] = 1;
+    }
+
+    return arr;
+  };
+
+  return (
+    <section>
+      <Typography variant="h1" gutterBottom>
+        Pascal
+      </Typography>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <Box sx={{ width: "40%" }}>
+          <Stack spacing={4}>
+            <Input placeholder="Input number of rows" value={rowCount} onChange={(e) => setRowCount(e.target.value)} />
+            <Button variant="contained" type="submit" onClick={() => setIsSubmit((prevState) => !prevState)}>
+              Tampilkan
+            </Button>
+          </Stack>
+        </Box>
+      </form>
+      <Box sx={{ marginY: 2 }}>
+        <p>
+          Output:
+          {isSubmit
+            ? pascalTriangle(rowCount).map((item) => {
+                return <li style={{ listStyle: "none" }}>{item}</li>;
+              })
+            : ""}
+        </p>
+      </Box>
+    </section>
+  );
+};
+export default Pascal;
